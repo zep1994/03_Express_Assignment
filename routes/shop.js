bf60c74d1491158125d27343af1c1d332c1c0ef2
zep1994 <snaxbags@protonmail.com>
2019-10-29 07:10:17 -0400

    add shop page

--- routes/shop.js
+++ routes/shop.js
@@ -3,12 +3,15 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
+const adminData = require('./admin')
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
   //res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-  res.render('shop')
+  const products = adminData.products
+  //render method: 2 args, 1: what to render, 2: data to render on 1
+  res.render('shop', {prods: products, docTitle: "Shop"}) //now prods is available in the view shop, we could pass in more than one value
 });
 
 module.exports = router;
