977c4b231ce8f91bac9e00c3fcedd05d83910f8d
zep1994 <snaxbags@protonmail.com>
2019-11-23 11:11:47 -0500

    Fix 404 in error controller

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const pageNotFound = require('./controllers/error')
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -20,8 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: "Page Not Found"})
-});
+app.use(pageNotFound.get404);
 
 app.listen(3000);
