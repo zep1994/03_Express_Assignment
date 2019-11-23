d4220bd701c74a94b10ce0fa9d0652e33209acd6
zep1994 <snaxbags@protonmail.com>
2019-11-23 16:28:18 -0500

    update bug

--- controllers/product.js
+++ controllers/product.js
@@ -1,39 +1,29 @@
-//temp place to store data
-const products = []
+const Product = require('../models/product');
 
-//Creates products on the fly - time to build a model
-//this still take the request (not using) , response object 
 exports.getAddProduct = (req, res, next) => {
-    res.render(
-      //the view that will be rendered
-      'add-product', 
-      //define the object that will hold the date that will be displayed
-      {
-      title: "Add-product", 
-      path: '/admin/add-product', 
-      pageTitle: "Add Product",
-      //active class is added
-      formCSS: true,
-      productsCSS: true,
-      activeAddProduct: true
-    });
-  }
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
+};
 
-  exports.postAddProduct = (req, res, next) => {
-    products.push({ title: req.body.title})
-    console.log(req.body.title)
-    res.redirect('/');
-  }
+exports.postAddProduct = (req, res, next) => {
+  const product = new Product(req.body.title);
+  product.save();
+  res.redirect('/');
+};
 
-  exports.getProducts = (req, res, next) => {
-    //This does not need to extracted becaue we moved our products array into the same file
-    //const products = adminData.products; //this will access the modules in the amdnin data with name of products
-    res.render('shop', {
-      prods: products, 
-      pageTitle: 'Shop', 
-      path: '/', 
-      hasProducts: products.length > 0,
-      activeShop: true,
-      ProductCSS: true
-    });
-  }
+exports.getProducts = (req, res, next) => {
+  const products = Product.fetchAll();
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
+};
--- models/product.js
+++ models/product.js
@@ -1,29 +1,15 @@
-/* One way to handle a model is to export a constructor function
-Old Gen
-
-module.exports = function Product() {
-
-}
-
-new Gen: 
-Create a class
-
-*/
-
-const products = []
+const products = [];
 
 module.exports = class Product {
     constructor(t) {
-        this.title = t
+        this.title = t;
     }
-    
+
     save() {
-        products.push(this)
+        products.push(this);
     }
 
-    //static makes sure you can call the class itself and not an instantiated object
     static fetchAll() {
-        return this.products
+        return products;
     }
-
 }
