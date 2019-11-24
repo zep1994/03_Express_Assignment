1da01215dc3a541299d5cee071916171b08f592e
zep1994 <snaxbags@protonmail.com>
2019-11-24 07:52:33 -0500

    add future views

--- controllers/product.js
+++ controllers/product.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('add-product', {
+  res.render('admin/add-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
     formsCSS: true,
@@ -17,13 +17,15 @@ exports.postAddProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  const products = Product.fetchAll();
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true
-  });
+  //call back procss for return to the function in the contoller for products, the function is referring to this anonynomus function 
+  Product.fetchAll((products) => {
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
+    });
+  });  
 };
--- models/product.js
+++ models/product.js
@@ -1,4 +1,22 @@
-const products = [];
+//save to a file
+//Model talks to database
+const fs = require('fs')
+const path = require('path')
+
+const p = path.join(
+    path.dirname(process.mainModule.filename), 
+    'data', 
+    'products.json'
+    )
+
+const getProuctsFromFile  = cb => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            return cb([]);
+        }
+            cb(JSON.parse(fileContent)) //call JSON parse to return as an array, this is async so it runs with undefined, must fix
+        })
+}
 
 module.exports = class Product {
     constructor(t) {
@@ -6,10 +24,15 @@ module.exports = class Product {
     }
 
     save() {
-        products.push(this);
-    }
+        getProuctsFromFile(products => {
+            products.push(this) //this refers to class because we used arrow function
+                fs.writeFile(p, JSON.stringify(products), (err) => {
+                    console.log(err)
+                })
+            })
+        }
 
-    static fetchAll() {
-        return products;
-    }
+    static fetchAll(cb) { //passing it a callback allows you pass a function, which will execute once fetchAll is done. this function will hold the data and will be aware of it when called
+        getProuctsFromFile(cb)
+        }
 }
