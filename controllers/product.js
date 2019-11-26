4bebca3552b7d96a675f69223a669c5ce60b43f8
zep1994 <snaxbags@protonmail.com>
2019-11-26 15:59:53 -0500

    setup routes

--- controllers/product.js
+++ controllers/product.js
@@ -1,20 +1,5 @@
 const Product = require('../models/product');
 
-exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
-  });
-};
-
-exports.postAddProduct = (req, res, next) => {
-  const product = new Product(req.body.title);
-  product.save();
-  res.redirect('/');
-};
 
 exports.getProducts = (req, res, next) => {
   //call back procss for return to the function in the contoller for products, the function is referring to this anonynomus function 
